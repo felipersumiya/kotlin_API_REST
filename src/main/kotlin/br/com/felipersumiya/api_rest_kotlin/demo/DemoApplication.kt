fc2525df782ad492b09de3a34a0580b02d9aef15
Felipe Roberto Sumiya <felipersumiya@gmail.com>
2022-03-10 14:51:55 -0300

    Primeiro Commit

---
+++ src/main/kotlin/br/com/felipersumiya/api_rest_kotlin/demo/DemoApplication.kt
@@ -0,0 +1,11 @@
+package br.com.felipersumiya.api_rest_kotlin.demo
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class DemoApplication
+
+fun main(args: Array<String>) {
+	runApplication<DemoApplication>(*args)
+}
---
+++ src/main/kotlin/br/com/felipersumiya/api_rest_kotlin/demo/entities/Contato.kt
@@ -0,0 +1,4 @@
+package br.com.felipersumiya.api_rest_kotlin.demo.entities
+
+class Contato {
+}
